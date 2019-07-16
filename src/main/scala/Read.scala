d008e7918e52ab21934558006cee2cc8d7d23f8d
Joshua <joshuagomersallgebba@gmail.com>
2019-07-16 10:34:06 +0100

    Take User Inputs Expanded Upon

--- src/main/scala/Read.scala
+++ src/main/scala/Read.scala
@@ -28,6 +28,33 @@ object Read {
     Thread.sleep(1000)
   }
 
+  def getOneByLength (idInput: Int ,nameLength: Int ,stringMatchType : String) :Unit = {
+    println(s"With The Parameters\n Exact Match \n Lenght:$nameLength \n We Found")
+    val db = Database.forConfig("mysqlDB")
+    val peopleTable = TableQuery[People]
+
+    def listPeople = {
+      val queryFuture = Future {
+        // simple query that selects everything from People and prints them out
+        db.run(peopleTable.result).map(_.foreach {
+          case (id, fName, lName, age) =>
+            stringMatchType match {
+              case "both" => if (fName.length == nameLength && lName.length == nameLength){println(s"Both First And LastName Match Length $id $fName $lName $age")}
+              case "fName" => if (fName.length == nameLength && lName.length != nameLength){println(s"The First Name Matchs Length $id $fName $lName $age")}
+              case "lName" => if (fName.length == nameLength && lName.length != nameLength){println(s"The First Name Matchs Length $id $fName $lName $age")}
+              case _ => println(s"")
+            }
+        })
+      }
+      Await.result(queryFuture, Duration.Inf).andThen {
+        case Success(_) =>  db.close()  //cleanup DB connection
+        case Failure(error) => println("Listing people failed due to: " + error.getMessage)
+      }
+    }
+    listPeople
+    Thread.sleep(1000)
+  }
+
   def getOne (idInput: Int ,fNameInput: String ,lNameInput: String ,ageInput: Int , exactCheck: Boolean = true) :Unit = {
     println(s"With The Parameters\n ${if (!exactCheck){"Similar Match"} else "Exact Match"}   \n Name:$fNameInput \n Surname:$lNameInput \n We Found")
     val db = Database.forConfig("mysqlDB")
--- src/main/scala/Testing.scala
+++ src/main/scala/Testing.scala
@@ -5,6 +5,21 @@ object Testing {
     takeUserInputs()
   }
 
+  def getOneLengthWithUserInputs():Unit = {
+    println("Enter The Length You Are Looking For In Whole Numbers")
+    val nameLength = scala.io.StdIn.readInt()
+    println("Enter Feild You Would Like To Search For Length In ,Both ,Forename ,Surname")
+    val searchType = scala.io.StdIn.readLine()
+    searchType.toLowerCase() match {
+      case "both" => Read.getOneByLength(1,nameLength,"both")
+      case "fname" => Read.getOneByLength(1,nameLength,"fName")
+      case "lname" => Read.getOneByLength(1,nameLength,"lName")
+    }
+
+
+    takeUserInputs()
+  }
+
   def getOneWithUserInputs():Unit = {
     println("Enter The First Name")
     val fNameSearch = scala.io.StdIn.readLine()
@@ -28,33 +43,89 @@ object Testing {
   def removeOneWithUserInputs():Unit = {
     println("Enter The First Name To Remove")
     val fNameInput = scala.io.StdIn.readLine()
-    println("Enter The Last Name To Add")
+    println("Enter The Last Name To Remove")
     val lNameInput = scala.io.StdIn.readLine()
-    println("Enter The Age To Add")
-    val age = scala.io.StdIn.readInt()
     Delete.deleteOne(fNameInput,lNameInput)
     takeUserInputs()
   }
 
   def takeUserInputs (): Unit = {
+    println("Select A Main Action \n1.Read \n2.Create \n3.Update \n4.Delete \n5.Exit")
+    val userMainMenu = scala.io.StdIn.readLine()
+    userMainMenu match {
+      case "1" => readMenu()
+      case "2" => addMenu()
+      case "3" => updateMenu()
+      case "4" => deleteMenu()
+      case "5" => println("Exit")
+        sys.exit()
+      case _ =>
+        println("Invalid Choice")
+        takeUserInputs()
+    }
 
-    println(s"1.ReadAll \n2.ReadOne \n3.Add User \n4.DeleteAll \n5.RemoveOne")
-    val userInput = scala.io.StdIn.readLine()
-    userInput match {
-      case "1" =>
-        Read.getAll()
-        takeUserInputs()
-      case "2" =>
-        getOneWithUserInputs()
-      case "3" =>
-        makeOneWithUserInputs()
-      case "4" =>
-        Delete.deleteAll()
-        takeUserInputs()
-      case "5" =>
-        removeOneWithUserInputs()
-      case _ => println("Invalid Try Again")
-        takeUserInputs()
+    def readMenu() {
+      println(s"1.ReadAll \n2.ReadOne \n3.ReadOne With Length \n4.<= Back \n5.Exit")
+      val userInput = scala.io.StdIn.readLine()
+      userInput match {
+        case "1" =>
+          Read.getAll()
+          takeUserInputs()
+        case "2" =>
+          getOneWithUserInputs()
+        case "3" =>
+          getOneLengthWithUserInputs()
+        case "4" =>
+          takeUserInputs()
+        case "5" =>
+          println("Exit")
+          sys.exit()
+        case _ => println("Invalid Try Again")
+          takeUserInputs()
+      }
+    }
+
+    def addMenu() {
+      println(s"1.Add User \n2.<= Back")
+      val userInput = scala.io.StdIn.readLine()
+      userInput match {
+        case "1" =>
+          makeOneWithUserInputs()
+          takeUserInputs()
+        case "2" =>
+          takeUserInputs()
+        case _ => println("Invalid Try Again")
+          takeUserInputs()
+      }
+    }
+    def updateMenu() {
+      println(s"1.Update A User \n2.<= Back")
+      val userInput = scala.io.StdIn.readLine()
+      userInput match {
+        case "1" =>
+          takeUserInputs()
+        case "2" =>
+          takeUserInputs()
+        case _ => println("Invalid Try Again")
+          takeUserInputs()
+      }
+    }
+
+    def deleteMenu() {
+      println(s"1.DeleteOne \n2.DeleteAll \n3.<= Back")
+      val userInput = scala.io.StdIn.readLine()
+      userInput match {
+        case "1" =>
+          removeOneWithUserInputs()
+          takeUserInputs()
+        case "2" =>
+          Delete.deleteAll()
+          takeUserInputs()
+        case "3" =>
+          takeUserInputs()
+        case _ => println("Invalid Try Again")
+          takeUserInputs()
+      }
     }
   }
 }
